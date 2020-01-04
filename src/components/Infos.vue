c6986109b80d8a3ea0b9b5c7f29f785ff87e043b
Andre Schmidt <andre.schmidt@blvcksmith.de>
2020-01-04 15:16:05 +0100

    info changed

--- src/components/Infos.vue
+++ src/components/Infos.vue
@@ -3,8 +3,6 @@
     <div class="flex justify-center items-end font-bold text-2xl">
       <font-awesome class="m-2" :icon="['fas', 'info']" size="1x"/>Anfahrt
     </div>
-    <br>Die Unterführung am Vechelder Bahnhof ist nur befahrbar für Fahrzeuge unter 2,30m Höhe.
-    <br>
     <br>Das Eingangstor ist Tag & Nacht geschlossen zu halten. Sie erhalten bei Anmeldung einen Schlüssel um jederzeit auf das Gelände zu kommen.
   </div>
 </template>
